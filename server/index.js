a4ff27631ac3b15ef0c1548a3ab15a8068e8f412
Kenrick <kenricklam98@gmail.com>
2021-11-28 17:29:53 -0800

    refactor hard coded connection string to Heroku config vars

--- server/index.js
+++ server/index.js
@@ -5,8 +5,10 @@ import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
-const app = express();
 dotenv.config();
+const app = express();
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
 
 // To parse the incoming requests with JSON payloads, but we need to limit 30mb of data for images sent to the server
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -20,12 +22,10 @@ app.get("/", (req, res) => {
     res.send("Hello to Story Time API");
 });
 
-const PORT = process.env.PORT || 5000;
-
 // connect to MongoDB
 mongoose
-    .connect(process.env.CONNECTION_URL)
+    .connect(uri)
     .then(() =>
         app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
     )
-    .catch((error) => console.log(error.message));
+    .catch((error) => console.log(error));
